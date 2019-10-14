a43866ba78b56a451c3353f3caedc64c76b37cff
CodeSadhu <patwardhanatharva6@gmail.com>
2019-10-14 09:42:03 +0530

    Contact Us not working

--- home/mainShop/admin.py
+++ home/mainShop/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 #Product - The class created in mainShop/models.py
-from . models import Product
+from . models import Product, Contact
 
 admin.site.register(Product)
+admin.site.register(Contact)
--- home/mainShop/views.py
+++ home/mainShop/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from . models import Product
+from . models import Product, Contact
 from math import ceil
 
 
@@ -27,6 +27,13 @@ def about(request):
 
 
 def contact(request):
+    if request.method == "POST":
+        name = request.POST.get('name', '')
+        email = request.POST.get('email', '')
+        phone = request.POST.get('phone', '')
+        desc = request.POST.get('desc', '')
+        contact = Contact(name=name, email=email, phone=phone, desc=desc)
+        contact.save()
     return render(request, 'mainShop/help.html')
 
 
