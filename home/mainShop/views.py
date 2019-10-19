7ddce3549195487932172ae2ff2d3bdc2346f7c3
CodeSadhu <patwardhanatharva6@gmail.com>
2019-10-19 20:22:30 +0530

    Product Tracking (Full Done | Working)

--- home/mainShop/views.py
+++ home/mainShop/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from . models import Product, Contact, Orders, OrderUpdate
 from math import ceil
+import json
 import logging
 
 
