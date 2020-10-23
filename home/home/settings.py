d91dc13972044a5a05c2a249544944ed53f42fe6
Atharva Patwardhan <patwardhanatharva6@gmail.com>
2020-10-23 23:51:07 +0530

    Procfile fixed. Settings.py edit - 1

--- home/home/settings.py
+++ home/home/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'home.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['home/templates'],
+        'DIRS': ['home/templates', 'mainShop/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
